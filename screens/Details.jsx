ed0d1906f776dfd20c0a68cd10d2208182407696
karam-zomlut <zomlutk@gmail.com>
2022-09-16 16:48:37 +0300

    Setup Screen Component

--- screens/Details.jsx
+++ screens/Details.jsx
@@ -1,9 +1,24 @@
-import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from 'react-native';
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  FocusedStatusBar,
+  DetailsDesc,
+  DetailsBid,
+} from '../components';
+
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from '../constants';
 
 const Details = () => {
-  return (
-    <Text>Details</Text>
-  )
-}
+  return <Text>Details</Text>;
+};
 
-export default Details
+export default Details;

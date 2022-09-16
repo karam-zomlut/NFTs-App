187625fb17fea779fb3d3b6c6064c0d4cea7eb86
karam-zomlut <zomlutk@gmail.com>
2022-09-16 10:53:09 +0300

    Add Constants Folder

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,6 +1,8 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { View, SafeArea, FlatList, Text } from 'react-native';
 
 const Home = () => {
+  const [ count, setCount ] = useState(0);
   return (
     <Text>Home</Text>
   )

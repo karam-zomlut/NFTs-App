5afc90bb6c11a468f6512359dfd3825041b128be
karam-zomlut <zomlutk@gmail.com>
2022-09-16 12:24:53 +0300

    Finish EndDate component

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,23 +1,26 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants';
 import { CircleButton } from './Buttons';
+import { NFTTitle, EthPrice, SubInfo } from './SubInfo';
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
   return (
-    <View style={{
-      backgroundColor: COLORS.white,
-      borderRadius: SIZES.font,
-      marginBottom: SIZES.extraLarge,
-      margin: SIZES.base,
-      ...SHADOWS.dark
-    }}>
-      <View style={{ width: "100%", height: 250 }}>
-        <Image 
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: '100%', height: 250 }}>
+        <Image
           source={data?.image}
-          resizeMode="cover"
+          resizeMode='cover'
           style={{
             width: '100%',
             height: '100%',
@@ -27,9 +30,10 @@ const NFTCard = ({ data }) => {
         />
         <CircleButton imgUrl={assets.heart} top={10} right={10} />
       </View>
-        {/* <Text>{data.name}</Text> */}
-    </View>
-  )
-}
 
-export default NFTCard
+      <SubInfo />
+    </View>
+  );
+};
+
+export default NFTCard;
--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as HomeHeader } from "./HomeHeader";
 export { default as NFTCard } from "./NFTCard";
 export { default as FocusedStatusBar } from "./FocusedStatusBar";
 export { CircleButton, ReactButton } from "./Buttons";
+export { NFTTitle, EthPrice, ImageCmp, People, EndDate, SubInfo } from "./SubInfo";

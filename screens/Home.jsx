a7884323427d9fc3691ee4c472ba0e99d9db6c20
karam-zomlut <zomlutk@gmail.com>
2022-09-16 11:25:22 +0300

    Setup NFTCard Cmoponent

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -9,6 +9,30 @@ const Home = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{flex: 1}}>
+        <View style={{zIndex: 0}}>
+          <FlatList 
+            data={NFTData}
+            renderItem={({item}) => <NFTCard data={item} />}
+            keyExtractor={item => item?.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+
+        <View style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+          zIndex: -1,
+        }}>
+          <View style={{height: 300, backgroundColor: COLORS.primary}} />
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }

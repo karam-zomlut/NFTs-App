7c7e45f86fa44e8c652a50db9a7008906fba2111
karam-zomlut <zomlutk@gmail.com>
2022-09-16 17:08:41 +0300

    Create RectButton on Details Page

--- screens/Details.jsx
+++ screens/Details.jsx
@@ -19,8 +19,28 @@ import { COLORS, SIZES, SHADOWS, FONTS, assets } from '../constants';
 
 const Details = ({ route, navigation }) => {
   const { data } = route.params;
-  console.log(data);
-  return <Text>Details</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent={true}
+      />
+
+      <View style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        paddingVertical: SIZES.font,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        zIndex: 1,
+      }}>
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Details;
